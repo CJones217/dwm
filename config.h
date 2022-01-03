cf629cb22451e3a48fccebb96747f3d1a75faf78
Cjones217 <coding.crap@tutanota.com>
2022-01-02 21:40:14 -0600

    add browser key

--- config.h
+++ config.h
@@ -65,6 +65,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
         { MODKEY,                       XK_grave,  spawn,          SHCMD("emoji")},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{MODKEY|ShiftMask,              XK_b,      spawn,          SHCMD("firefox")},
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
